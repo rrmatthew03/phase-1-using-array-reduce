ed542ee3366fa460babdd8126b882f07c860ce72
ryan rivera <rrmatthew03@yahoo.com>
2022-04-10 18:49:40 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const sinon = require( 'sinon' )
+
+
+function totalBatteries(){
+    console.log(totalBatteries)
+}
+
+describe('reducer', function() {
+  describe('batteries', function() {
+    it('should have a `totalBatteries` variable', function() {
+      expect(totalBatteries).to.exist;
+    });
+
+    it('should have a number as a result', function() {
+      expect(totalBatteries).to.be.a('number');
+    });
+
+    it('should have made the sum of all the assembled batteries', function() {
+      expect(totalBatteries).to.eql(31);
+    });
+  });
+});
